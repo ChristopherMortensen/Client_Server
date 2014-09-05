165a49faf4e08c0dd4e7be1d8954157caa927265
ChristopherMortensen <christophervip@gmail.com>
2014-09-05 19:09:23 +0200

    ExampleClientServer.Client.java updated. New package
    Created a socket object in client.java.
    New empty package created, just to a youtube-exercise.

--- ClientServerExample/src/ExampleClientServer/Client.java
+++ ClientServerExample/src/ExampleClientServer/Client.java
@@ -1,5 +1,12 @@
 package ExampleClientServer;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     
+    public static void main(String[] args) throws IOException {
+        //==Instantiate a Socket object, so we have a socket we can send to
+         Socket socket = new Socket("localhost", 4711);
+    }
 }
